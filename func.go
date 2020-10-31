1052abe7bf4b1b540803701a5363efb04876abff
mizugoro <mizugoro0121@outlook.jp>
2020-10-31 21:33:02 +0900

    エラー処理を追加

--- func.go
+++ func.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 func sum(i, j int) {
 	//引数がある場合は変数と型を指定します。複数の同じ型が続く場合は型の宣言は一つにまとめることができます
@@ -13,9 +17,44 @@ func sum(i, j int) {
 func swap(i, j int) (int, int) {
 	return j, i
 }
+
+//自作のエラーはerrorsパッケージを用いて作ることができる
+func div(i, j int) (int, error) {
+	if j == 0 {
+		//自作のエラーを返す
+		return 0, errors.New("divided by zero")
+	}
+	return i / j, nil
+}
+
 func main() {
 	sum(2, 3) //5
 	x, y := 3, 4
 	x, y = swap(x, y)
 	fmt.Println(x, y)
+
+	x, _ = swap(x, y) //第二戻り値を無視する
+
+	//関数が多値を返せることを利用して内部で発生したエラーを戻り値で表現します
+	//処理に成功した場合はエラーはnilにし、異常があった場合はエラーだけに値が入り他方はゼロ値になります
+	/*
+		, err := os.Open("print.go")
+		if err != nil {
+			//エラー処理
+
+		}
+		//fileを用いた処理
+	*/
+	//関数リテラルを用いると無名関数を作ることができる
+	func(i, j int) {
+		fmt.Println(i + j)
+	}(2, 4)
+
+	n, err := div(10, 0)
+	if err != nil {
+		//エラーを出力しプログラムを終了する
+		log.Fatal(err)
+	}
+	fmt.Println(n)
+
 }
