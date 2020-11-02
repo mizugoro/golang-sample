02c7ee0bb06a0af4c927edfcac694e3be30b5c69
mizugoro <mizugoro0121@outlook.jp>
2020-11-02 09:31:28 +0900

    ポインタ型について記述

--- struct.go
+++ struct.go
@@ -8,15 +8,22 @@ type Task struct {
 	done   bool
 }
 
+func Finish(task *Task) {
+	task.done = true
+}
+
 func main() {
-	var task Task = Task{
-		ID:     1,
-		Detail: "buy the milk",
-		done:   true,
-	}
-	fmt.Println(task.ID)     //1
-	fmt.Println(task.Detail) //buy the milk
-	fmt.Println(task.done)   //true
+	//var task Task = Task{1, "buy the milk", true} //Task型
+	var task *Task = &Task{done: false} //Taskのポインタ型
+	//構造体型もアドレスを取得し、ポインタ型で扱うことができる。変数には構造体の値ではなくアドレスが格納される
+	//関数に対して構造体を値渡しするとデータはコピーされるため変更は反映されない
+	Finish(task)
+	fmt.Println(task.done) //true
+	/*
+		fmt.Println(task.ID)     //1
+		fmt.Println(task.Detail) //buy the milk
+
+	*/
 
 	//構造体の生成時に値を明示的に指定しなかった場合はゼロ値で初期化されます
 }
