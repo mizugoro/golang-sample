41ca6286a03b86df8e77f5ecccd8721a795a5df9
mizugoro <mizugoro0121@outlook.jp>
2020-11-02 10:00:20 +0900

    new関数を用いて初期化

--- struct.go
+++ struct.go
@@ -14,16 +14,18 @@ func Finish(task *Task) {
 
 func main() {
 	//var task Task = Task{1, "buy the milk", true} //Task型
-	var task *Task = &Task{done: false} //Taskのポインタ型
+	var task *Task = new(Task) //Taskのポインタ型
+
 	//構造体型もアドレスを取得し、ポインタ型で扱うことができる。変数には構造体の値ではなくアドレスが格納される
 	//関数に対して構造体を値渡しするとデータはコピーされるため変更は反映されない
 	Finish(task)
 	fmt.Println(task.done) //true
-	/*
-		fmt.Println(task.ID)     //1
-		fmt.Println(task.Detail) //buy the milk
 
-	*/
+	fmt.Println(task.ID)     //1
+	fmt.Println(task.Detail) //buy the milk
 
 	//構造体の生成時に値を明示的に指定しなかった場合はゼロ値で初期化されます
+
+	//new()を用いて初期化することができる
+	//new()は構造体のフィールドをすべてゼロ値で初期化し、そのポインタを返す
 }
