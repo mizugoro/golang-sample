36529da5e5f9227957d51f8f678aba726ea105af
mizugoro <mizugoro0121@outlook.jp>
2020-11-06 13:38:01 +0900

    error

--- net-http.go
+++ net-http.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
+
+type Person struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "hello world")
 }
+func PersonHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close() //処理の最後にBodyを閉じる
+
+	if r.Method == "POST" {
+		//リクエストボディをJSONに変換
+		var person Person
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&person)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//ファイル名を{id}.txtとする
+		filename := fmt.Sprint("%d.txt", person.ID)
+		file, err := os.Create(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		//ファイルにNameを書き込む
+		_, err = file.WriteString(person.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//
+		w.WriteHeader(http.StatusCreated)
+	}
+}
 
 func main() {
-	http.HandleFunc("/", IndexHandler) //ルーティングの設定
+	http.HandleFunc("/", IndexHandler)         //ルーティングの設定
+	http.HandleFunc("/persons", PersonHandler) //ルーティングの設定
 	/*func HandleFunc(pattern string,
 		handler func(ResponsWriter, *Request))
 
