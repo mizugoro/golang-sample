6b61e049ea1a4356f2004818f911b2550e0afc4d
mizugoro <mizugoro0121@outlook.jp>
2020-12-05 15:40:41 +0900

    Make CLI tool

--- app/main.go
+++ app/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/nasum/culc/cmd"
+)
 
 func main() {
-	fmt.Println("error")
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(-1)
+	}
 }
